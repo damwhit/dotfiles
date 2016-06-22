e829cf4fb26c108563e98aff815e3be260410b18
David Whitaker <damwhit@gmail.com>
2016-06-21 22:30:42 -0600

    add .emacs.d

---
+++ .emacs.d/elpa/seeing-is-believing-20160602.718/seeing-is-believing-autoloads.el
@@ -0,0 +1,16 @@
+;;; seeing-is-believing-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("seeing-is-believing.el") (22378 4854
+;;;;;;  253849 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; seeing-is-believing-autoloads.el ends here
