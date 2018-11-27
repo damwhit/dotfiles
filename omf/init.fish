508eb024c9da97aa486cf939b24a02099e37880d
David Whitaker <damwhit@gmail.com>
2018-11-26 19:06:43 -0700

    Add yarn global to install location to path

--- omf/init.fish
+++ omf/init.fish
@@ -2,11 +2,11 @@ alias home='cd ~'
 alias be='bundle exec'
 alias gcam='git commit -m'
 
-# ruby
 export PATH="$HOME/.rbenv/shims:$PATH"
 
-# everything else
 export PATH="/usr/local/bin:$PATH"
 
 export NVM_DIR="/Users/davidwhitaker/.nvm"
+
+export PATH="$(yarn global bin):$PATH"
 
